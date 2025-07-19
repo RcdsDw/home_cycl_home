9b842819a65ef2bfa7da1651fa742656db941c20
Rafael CDS <rafael.candidodasilva.pro@gmail.com>
2025-07-19 18:20:03 +0200

    update time of token auth

--- home_cycl_home_front/src/actions/api.js
+++ home_cycl_home_front/src/actions/api.js
@@ -1,6 +1,7 @@
 const API_URL = process.env.REACT_APP_API_URL;
 
 async function api(method, endpoint, payload = null, token = true) {
+    console.log(localStorage.getItem('token'))
     const headers = {
         'Content-Type': 'application/json',
         ...(token && { 'Authorization': `Bearer ${localStorage.getItem('token')}` }),
